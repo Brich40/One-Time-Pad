734c8992c15961dab4dd64624643742adfed3700
Brich40 <brich.oussama@gmail.com>
2021-01-08 18:56:43 +0100

    Add encryption and decryption

--- main.py
+++ main.py
@@ -15,11 +15,27 @@ def write_file(path, text):
     f.close()
 
 
+def get_random_number(size):
+    """
+    :param size: with of the random int
+    :return: random int
+    randint() function uses /dev/random, I used this code to get the random manually :
+
+    <with open("/dev/random", 'rb') as f:
+        random_number = int.from_bytes(f.read(12), 'big')>
+
+    but it takes a long time to get it.
+    """
+    min_int = pow(10, size-1)
+    max_int = pow(10, size) - 1
+    return random.randint(min_int, max_int)
+
+
 def generate_pads(path):
     """
     Generate pads to be used to encrypt text
     :param path: path to parent directory
-    :return: N/A
+    :return new_dir_name: new pads dir path
     """
     # Create new dir taking in consideration the olds ones
     old_dirs = os.listdir(path)
@@ -41,8 +57,9 @@ def generate_pads(path):
             break
 
     for j in range(100):
-        # Get a random number 12 digit = 48 bytes
-        random_number = random.randint(100000000000, 999999999999)
+
+        # Get a random number from /dev/random 12 digit = 48 bytes
+        random_number = get_random_number(12)
 
         p_path = new_dir_name + "/" + str(j).zfill(2) + "p"
         s_path = new_dir_name + "/" + str(j).zfill(2) + "s"
@@ -52,6 +69,57 @@ def generate_pads(path):
         write_file(s_path, str(random_number))
         write_file(c_path, str(random_number))
 
+    return new_dir_name
+
+def encrypt(string, pad_path):
+    """
+    Encrypt srting using pads
+    :param string: string to be encrypted
+    :param pad_path: pad path
+    :return:
+    """
+    f = open(pad_path, "r")
+    pads = f.read()
+    encrypted_str = ""
+
+    for char in string:
+        if not pads[0]:
+            print("Pad is empty!")
+            return ""
+
+        # Get pad value and remove it from pads
+        pad_value = int(pads[0])
+        pads = pads[1:]
+        char_ord = ord(char)
+        encrypted_str = encrypted_str + str(chr(char_ord + pad_value))
+
+    return encrypted_str
+
+
+def decrypt(string, pad_path):
+    """
+    Decrypt string using pad
+    :param string: string to be decryted
+    :param pad_path:
+    :return:
+    """
+    f = open(pad_path, "r")
+    pads = f.read()
+    decrypted_str = ""
+
+    for char in string:
+        if not pads[0]:
+            print("Pad is empty!")
+            return ""
+
+        # Get pad value and remove it from pads
+        pad_value = int(pads[0])
+        pads = pads[1:]
+        char_ord = ord(char)
+        decrypted_str = decrypted_str + str(chr(char_ord - pad_value))
+
+    return decrypted_str
+
 
 def main():
     """
@@ -59,8 +127,18 @@ def main():
     :return: status
     """
 
-    pads_path = "./pads/"
-    generate_pads(pads_path)
+    pads_path = "./pads"
+    pad = "./pads/0000/00c"
+    # generate_pads(pads_path)
+
+    test_str = "test"
+    print("Test word : " + test_str)
+    encrypted_str = encrypt("Test", pad)
+    print(encrypted_str)
+
+    decrypted_str = decrypt(encrypted_str, pad)
+    print(decrypted_str)
+
     return 0
 
 
